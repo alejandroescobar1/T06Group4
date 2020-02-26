3b63f56207f559f97c0077d4dbe594fe5ac777b5
PeterSun123 <60020375+PeterSun123@users.noreply.github.com>
2020-02-25 20:55:29 -0700

    Add files via upload

--- Maze.java
+++ Maze.java
@@ -12,7 +12,7 @@ public class Maze {
 	 private boolean[][] up;
 	 private boolean[][] right;
 	 private boolean[][] left;
-
+	 private boolean[][] visited;
 
 	 
 	 public Maze(int length, int width) {
@@ -40,6 +40,8 @@ public class Maze {
 	                left[x][y]  = true;
 	            }
 	        }
+	        up[0][0]=false;
+	        down[length-1][width-1]=false;
 			//convert to 2D Array
 			CoordinateList2D=new char[length][width];
 			for (int row=0;row<width;row++) {
@@ -49,29 +51,46 @@ public class Maze {
 		 GenerateWalls();
 
 		 }
-	 
+	 public boolean SameSet() {
+		 
+	 }
 	 public void GenerateWalls()
 	 {
 				//generate a random integer between 1-size of maze( directions)
-				Random ran = new Random();
+				//Random ran = new Random();
 				int Welldown=0;
 				//using the random number above, 1: check left(x-1), 2:right(x+1),3:up(y+1) 4:down(y-1)
 				//for (int row=0;row<this.width;row++) {
 				//for (int col =0;col<this.length;col++) {
-				while(Welldown<length*width-1) {
+				//while(Welldown<(length*width-1)) {
+				//while any of the internal walls has true value
+					while(Welldown<(length*width+30000)) {	
 					//0-4 for length of 5
+						Random ran = new Random();
 					int randomrow=ran.nextInt(length);
 					int randomcol=ran.nextInt(width);
 					int randomdir=ran.nextInt(4);
 					if (randomdir==0 && randomrow+1<width) {
 					if (CoordinateList2D[randomrow][randomcol]!=CoordinateList2D[randomrow+1][randomcol]&&
 							down[randomrow][randomcol]==true&&up[randomrow+1][randomcol]==true) {
+						//if there's more to the set, do it to the rest 
+						/*for (int row=0;row<width;row++) {
+							for (int col =0;col<length;col++) {
+							 if (CoordinateList2D[row][col]==CoordinateList2D[randomrow+1][randomcol]&&
+								row==randomrow+1) {;}
+							}}*/
+						
+						
 						CoordinateList2D[randomrow+1][randomcol]=CoordinateList2D[randomrow][randomcol];
-							//knock down the up wall of randomrow+1 and south wall of randomrow(same)
-							down[randomrow][randomcol]=false;
-							up[randomrow+1]	[randomcol]=false;	
-							Welldown++;
-							}}
+
+							//knock down the up wall of randomrow+1 and down wall of randomrow(same)
+
+						down[randomrow][randomcol]=false;
+						up[randomrow+1]	[randomcol]=false;	
+							
+							}
+
+					Welldown++;}
 					
 				if (randomdir==1&&(randomcol+1)<length) {
 				if (CoordinateList2D[randomrow][randomcol]!=CoordinateList2D[randomrow][randomcol+1]&&
@@ -79,29 +98,32 @@ public class Maze {
 						
 					CoordinateList2D[randomrow][randomcol+1]=CoordinateList2D[randomrow][randomcol];
 						//knock down the right wall of randomcol and left wall of randomcol+1(same)
-						right[randomrow][randomcol]=false;
-						left[randomrow]	[randomcol+1]=false;	
-						Welldown++;}
+					right[randomrow][randomcol]=false;
+					left[randomrow]	[randomcol+1]=false;
 						}
+	
+				Welldown++;}
 				if (randomdir==2&&randomrow-1>=0) {
 				if (CoordinateList2D[randomrow][randomcol]!=CoordinateList2D[randomrow-1][randomcol]&&
-						right[randomrow-1][randomcol]==true&&left[randomrow][randomcol]==true) {
-					CoordinateList2D[randomrow-1][randomcol]=CoordinateList2D[randomrow][randomcol];
-				//knock down the right wall of randomrow-1 and left wall of randomrow(same)
-				right[randomrow-1][randomcol]=false;
-				left[randomrow][randomcol]=false;					
-						Welldown++;}
-						}
-				
-				if (randomdir==3&&randomrow-1>=0) {
-				if (CoordinateList2D[randomrow-1][randomcol]!=CoordinateList2D[randomrow][randomcol]&&
 						down[randomrow-1][randomcol]==true&&up[randomrow][randomcol]==true) {
 					CoordinateList2D[randomrow-1][randomcol]=CoordinateList2D[randomrow][randomcol];
-				//knock down the down wall of randomrow-1 and up wall of randomrow(same)
-				down[randomrow-1][randomcol]=false;
-				up[randomrow][randomcol]=false;		
-						Welldown++;}
+				//knock down the right wall of randomrow-1 and left wall of randomrow(same)
+					down[randomrow-1][randomcol]=false;
+					up[randomrow][randomcol]=false;		
 						}
+
+				Welldown++;}
+				
+				if (randomdir==3&&randomcol-1>=0) {
+				if (CoordinateList2D[randomrow][randomcol-1]!=CoordinateList2D[randomrow][randomcol]&&
+						right[randomrow][randomcol-1]==true&&left[randomrow][randomcol]==true) {
+					CoordinateList2D[randomrow][randomcol-1]=CoordinateList2D[randomrow][randomcol];
+				//knock down the down wall of randomrow-1 and up wall of randomrow(same)
+					right[randomrow][randomcol-1]=false;
+					left[randomrow][randomcol]=false;	
+						}
+
+				Welldown++;	}
 				
 					} 
 				System.out.println(Welldown);
