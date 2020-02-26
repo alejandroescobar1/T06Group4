3c2692a043c684fec7f08ea44ae95b6442889df7
eugenioangelica <60020396+eugenioangelica@users.noreply.github.com>
2020-02-25 17:55:48 -0700

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,13 +1,113 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Player {
-	// The lives the player has. This will be edited in the enemies and item class
-	int lives = 3;
-	int livesChange = 0;
-	public int updateLives(int lives, int livesChange) {
-		lives = lives + livesChange;
-		livesChange = 0;
+	private int lives = 3;
+	private String playerName= "Dave";
+	private char direction;
+	private int steps; 
+	
+
+	public void setLives(int newLives)
+	{
+		this.lives = newLives;
+	}
+	
+	public int getLives()
+	{
 		return lives;
+	}
+	
+	public int updateLives(int livesChange)
+	{
+		return (this.lives = lives + livesChange);
+	}
+	
+	//////////////////////////////////////////////////////////////////////
+	// This code allows the user to name the avatar's name. The default name is Dave
+	public void setPlayerName(String inputName)
+	{
+		if (inputName != null)
+		{
+			if(inputName.isEmpty() == false) 
+			this.playerName = inputName;
+		}
+	}
+	
+	public String getPlayerName()
+	{
+		return (this.playerName);
+	}
+	
+	public void setDirection(char newDirection)
+	{
+		this.direction = newDirection;
+	}
+	
+	public char getDirection()
+	{
+		return (this.direction);
+	}
+	
+	public void setSteps(int newSteps)
+	{
+		this.steps = newSteps;
+	}
+	
+	public int getSteps()
+	{
+		return (this.steps);
+	}
+	
+	
+	public void move(char newDirection, int newSteps)
+	{
+		if (direction == 'w')
+		{
+			
+		}
+		
+		if (direction == 's')
+		{
+			
+		}
+		
+		if (direction == 'd')
+		{
+			
+		}
+		
+		if (direction == 'a')
+		{
+			
+		}
+	}
+	
+	
+	public static void main (String[] args)//
+	{
+		Scanner playerInfo = new Scanner(System.in);//
+		
+		System.out.println("Enter the name of the player: ");//
+		String inputName = playerInfo.nextLine();//
+		
+		
+		System.out.println("Enter the player's move (Please enter either w, s, d or a) :");
+		char move = playerInfo.next().charAt(0);
+		
+		System.out.println("Enter the amount of steps:");
+		int amountOfSteps = playerInfo.nextInt();
+		
+	    Player player1 =  new Player();
+	    player1.setPlayerName(inputName);
+	    player1.setDirection(move);
+	    player1.setSteps(amountOfSteps);
+	    
+	    
+	    
+	    System.out.println(player1.getPlayerName());
+	
 	}
 	
 	//////////////////////////////////////////////////////////////////////
@@ -16,84 +116,62 @@ public class Player {
 	// the jackal ring to 3 and the staff to 4. The amulet sets it to value associated with the 
 	// item it randomly changes to. 
 	int objectEquiped = 0;
-	
+
 	// This should be in Items class
-	
-	if (Player.coor() == Gem.coor()) {
+
+	if (Player.coor() == Gem.coor()) 
+	{
 		objectEquiped = 1;
 		// call update score function in Score class
 		updateScore(objectEquiped);
-		
 	}
-	
-	// This is a draft of the updateScore function
-	public int updateScore(objectEquiped) {
+		
+		// This is a draft of the updateScore function
+	public int updateScore(objectEquiped) 
+	{
 		int score = 0;
-		if (objectEquiped == 0) {
+		if (objectEquiped == 0) 
+		{
 			return null;
 		}
-		// in the case of a gem, there is no timer to the effect
-		else if (objectEquiped == 1) {
+			// in the case of a gem, there is no timer to the effect
+		else if (objectEquiped == 1)
+		{
 			score = score + 100;
 			objectEquiped = 0;
 			return score;
 		}
 		// in the case of the scarab jewl and jakal ring there is no timer to the effect, 
 		// but there is an additional step for the updating of lives
-		else if (objectEquipped == 2) {
+		else if (objectEquipped == 2) 
+		{
 			livesChange = 1;
 			updateLives(lives, livesChange);
 			score = score + 200;
 			objectEquiped = 0;
 			return score;
 		}
-		else if (objectEquipped == 3) {
+		else if (objectEquipped == 3) 
+		{
 			livesChange = -1;
 			updateLives(lives, livesChange);
 			score = score - 200;
 			objectEquiped = 0;
 			return score;
 		}
-		else if (objectEquipped == 4) {
-			
+		else if (objectEquipped == 4)
+		{
+				
 		}
 	}
-	
-	
-	
-	//////////////////////////////////////////////////////////////////////
-	// This code allows the user to name the avatar's name. The default name is Dave
-	String playerName = "Dave";
-	public void setName(String name) {
-		String newPlayerName = name;
-		this.playerName = newPlayerName;
-	}
+		
 	//////////////////////////////////////////////////////////////////////
 	// This getter method gets the location of the player from the maze class
-	public Location getlocationCoordinate() {
+	public Location getlocationCoordinate()
+	{
 		ArrayList<Integer> playerLocation = new ArrayList();
-		}
-	
-	/////////////////////////////////////////////////////////////////////
-	// This method moves the player. The setter functions it invokes must be defined in the 
-	// maze class
-	String direction = "N";
-	public ArrayList<Integer> move(direction){
-		// Angelica will write a getter function and in this spot, she will call it.
-		// this getter function actually just takes user input
-		
-		if (direction == "w") {
-			setVerticalChange(1);
-		}
-		if (direction == "s") {
-			setVerticalChange(-1);
-		}
-		if (direction == "d") {
-			setHorizontalChange(1);
-		}
-		if (direction == "a") {
-			setHorizontalChange(-1);
-		}
 	}
+
 }
 	
+
