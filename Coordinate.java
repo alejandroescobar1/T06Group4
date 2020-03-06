171186076270e53d17f4d0b604881186f2bc0772
alejandroescobar1 <60020418+alejandroescobar1@users.noreply.github.com>
2020-03-06 08:54:47 -0700

    Add files via upload

--- Coordinate.java
+++ Coordinate.java
@@ -1,50 +1,128 @@
+import java.util.ArrayList;
 public class Coordinate {
 	private int xCoord;
 	private int yCoord;
 	private char status;
-	private char Letter;
-	public Coordinate(int newX,int newY,char newStatus) {
+	private char letter;
+	private boolean up;
+	private boolean down;
+	private boolean left;
+	private boolean right;
+	
+	
+	public Coordinate(int col,int row,char newStatus,char newletter, boolean newup,boolean newdown,boolean newleft,boolean newright) {
 		this.setStatus(newStatus);
-		this.setX(newX);
-		this.setY(newY);
-
+		this.setX(col);
+		this.setY(row);
+		this.letter = newletter;
+		this.up = newup;
+		this.down = newdown;
+		this.left = newleft;
+		this.right = newright;
 	}
+	
+	
 	public Coordinate(Coordinate Copy) {
 		this.xCoord = Copy.xCoord;
 		this.yCoord = Copy.yCoord;
 		this.status = Copy.status;
-		this.Letter = Copy.Letter;		
+		this.letter = Copy.letter;
+		this.up = Copy.up;
+		this.down = Copy.down;
+		this.left = Copy.left;
+		this.right = Copy.right;
 	}
+	
+	
 	public void setX(int newX) {
 		this.xCoord = newX;
 	}
+	
+	
 	public void setY(int newY) {
 		this.yCoord = newY;
 	}
+	
+	
 	public void setStatus(char newStatus) {
 		this.status = newStatus;
 	}
 	
+	
 	public void setLetter(char newLetter) {
-		this.Letter = newLetter;
+		this.letter = newLetter;
 	}
+	
+	
+	
+	public void setUp(boolean newUp) {
+		this.up = newUp;
+	}
+	
+	
+	public void setDown(boolean newDown) {
+		this.down = newDown;
+	}
+	
+	
+	public void setLeft(boolean newLeft) {
+		this.left = newLeft;
+	}
+	
+	
+	public void setRight(boolean newRight) {
+		this.right = newRight;
+	}
+	
+	
+	
 	public int getX() {
 		Coordinate copy = new Coordinate(this);
 		return copy.xCoord;
 	}
+	
+	
 	public int getY() {
 		Coordinate copy = new Coordinate(this);
 		return copy.yCoord;
 	}
+	
+	
 	public char getStatus() {
 		Coordinate copy = new Coordinate(this);
 		return copy.status;
 	}
+
 	
 	public char getLetter() {
-		Coordinate copy = new Coordinate(this);
-		return copy.Letter;
+		Coordinate Copy = new Coordinate(this);
+		return Copy.letter;
 	}
+	
+	
+	public boolean getUp() {
+		Coordinate Copy = new Coordinate(this);
+		return Copy.up;
+	}
+	
+	
+	public boolean getDown() {
+		Coordinate Copy = new Coordinate(this);
+		return Copy.down;
+	}
+	
+	
+	public boolean getLeft() {
+		Coordinate Copy = new Coordinate(this);
+		return Copy.left;
+	}
+	
+	
+	public boolean getRight() {
+		Coordinate Copy = new Coordinate(this);
+		return Copy.right;
+	}
+	
 	
 	public boolean statusCheck(char check) {
 		if (this.getStatus() == check){
@@ -54,38 +132,56 @@ public class Coordinate {
 			return false;
 		}
 	}
-	//RENAME TO "XADJACENT"
-	public boolean xCollision(Coordinate coord) {
-		if ((this.getY() == coord.getY())&&((this.getX() == (coord.getX() - 1)) || (this.getX() == (coord.getX() + 1)))){
-			return true;
+	
+	
+	public boolean checkWall(int direction) {
+		if (direction == 0) {
+			return this.getDown();
 		}
-		else{
-			return false;
+		else if (direction == 1) {
+			return this.getRight();
+		}
+		else if (direction == 2) {
+			return this.getUp();
+		}
+		else if (direction == 3) {
+			return this.getLeft();
+		}
+		else { return true;}
+	}
+	
+	public void breakWall(int Direction) {
+		if (Direction == 0) {
+			this.setUp(false);
+		}
+		else if (Direction == 1) {
+			this.setRight(false);	
+			}
+		else if (Direction == 2) {
+			this.setDown(false);		
+			}
+		else if (Direction == 3) {
+			this.setLeft(false);		
 		}
 	}
-	public boolean yCollision(Coordinate coord) {
-		if ((this.getX() == coord.getX())&&((this.getY() == (coord.getY() - 1)) || (this.getY() == (coord.getY() + 1)))){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
+	
+	
 	public String toString() {
-		return "Coordinate: ("+this.xCoord+","+this.yCoord+")"+" Status:"+ this.status+" Letter:"+ this.Letter;
+		return "Coordinate: ("+this.xCoord+","+this.yCoord+")"+" Status:"+ this.status;
 	}
 	public static void main(String[] args) {
-		Coordinate newcoord = new Coordinate(1,1,'e');
-		System.out.println(newcoord.toString());
-		Coordinate coord2 = new Coordinate(newcoord);
-		coord2.setStatus('w');
-		System.out.println(newcoord.toString());
-		System.out.println(coord2.toString());
-		coord2.setX(2);
-		System.out.println(newcoord.xCollision(coord2));
-		coord2.setX(3);
-		System.out.println(newcoord.xCollision(coord2));
-		System.out.println(newcoord.statusCheck('e'));
-		System.out.println(newcoord.statusCheck('w'));
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- Maze.java
+++ Maze.java
@@ -1,34 +1,38 @@
 import java.util.*; 
 
 public class Maze {
-	 private int length;
-	 private int width;
+	 static private int length;
+	 static private int width;
 	 private  ArrayList<Coordinate> CoordinateList;
-
-	 public static char[][] CoordinateList2D;
+	 private ArrayList<ArrayList<Coordinate>> CoordinateList2D2;
+	 private static char[][] CoordinateList2D;
 	 
+	 
+	 private boolean[][] down;
+	 private boolean[][] up;
+	 private boolean[][] right;
+	 private boolean[][] left;
+	 private boolean[][] visited;
 
-	 public static boolean[][] down;
-	 public static boolean[][] up;
-	 public static boolean[][] right;
-	 public static boolean[][] left;
-	 public static boolean[][] visited;
-
-	 private static String[][] LetterWitWalls;
 	 
 	 public Maze(int length, int width) {
 		 this.CoordinateList=new ArrayList<Coordinate> ();
+		 this.CoordinateList2D2 = new ArrayList<ArrayList<Coordinate>>();
 		 this.length=length;
 		 this.width=width;
+		 char newLetter = 65;
 		 for(int i=0;i<(length);i++) {
 			 for(int j=0;j<(width);j++) {
-				 this.CoordinateList.add(new Coordinate(i,j,'w'));
+				 Coordinate tempCoor = new Coordinate(j,i,'e',newLetter,true,true,true,true);
+				 this.CoordinateList.add(tempCoor);
+				 ArrayList<Coordinate> tempList = new ArrayList<Coordinate>();
+				 tempList.add(tempCoor);
+				 this.CoordinateList2D2.add(tempList);
+				 if (newLetter > 89) {newLetter = 65;}
+				 else {newLetter++;}
 			 }}
 		 //randomly generate wall i.e. change 'e' to 'w' but have to be sure the wall doesn't form
 		 //a closed path, i.e. at least one of direction should be opened(for entering)
-			for (int i=0;i<CoordinateList.size();i++) {
-				 CoordinateList.get(i).setLetter('E');
-			}
 		 down = new boolean[length][width];
 		 up = new boolean[length][width];
 		 right = new boolean[length][width];
@@ -48,65 +52,24 @@ public class Maze {
 			for (int row=0;row<width;row++) {
 			for (int col =0;col<length;col++) {
 				CoordinateList2D[row][col]=CoordinateList.get(row*length+col).getLetter();}}
-			right[0][0] = false;
-			right[0][1] = false;
-			down[0][1] = false;
-			up[1][1] = false;
-			right[1][0] = false;
-			down[1][0] = false;
-			up[2][0] = false;
-			down[2][0] = false; 
-			up[3][0] = false;
-			right[3][0] = false;
-			down[3][1] = false;
-			up[4][1] = false;
-			right[4][0] = false; 
-			down[4][0] = false; 
-			up[5][0] = false;
-			right[5][0]= false;
-			right[5][1] = false;
-			up[5][2] = false;
-			down[4][2] = false;
-			right[4][2] = false;
-			down[4][3] = false; 
-			up[5][3] = false;
-			right[1][1] = false;
-			right[1][2] = false;
-			down[1][2] = false;
-			up[2][2] = false; 
-			right[2][1] = false;
-			down[2][2] = false;
-			up[3][2] = false;
-			right[3][2] = false;
-			right[0][2] = false; 
-			right[0][3] = false;
-			down[0][3] = false;
-			up[1][3] = false;
-			right[1][3] = false;
-			right[1][4] = false;
-			up[1][5] = false;
-			down[0][5] = false;
-			down[1][5] = false;
-			up[2][5] = false;
-			down[2][5] = false;
-			up[3][5] = false;
-			right[3][4] = false;
-			up[3][4] = false;
-			down[2][4] = false;
-			right[2][3] = false;
-			right[4][4] = false;
-			down[4][5] = false;
-			up[5][5] = false;
-			down[3][5] = false;
-			up[4][5] = false;
-			right[4][4] = false;
-			down[4][4] = false;
-			up[5][4] = false;
-			CoordinateList2D[0][0] = 'P';
+			
+			//Hardcoded Walls and Coordinate List
+				//ArrayList<Coordinate> temp = new ArrayList<Coordinate>();
+			//for (ArrayList<Coordinate> x :this.CoordinateList2D2) {
+			///	if (x.contains(temp)){
+					
+			//	}
+				
+			}
+			
+			
+			
 		 //GenerateWalls();
-
-		 }
-
+		
+		 
+	 public boolean SameSet() {
+		 return true;
+	 }
 	 public void GenerateWalls()
 	 {
 				//generate a random integer between 1-size of maze( directions)
@@ -117,169 +80,246 @@ public class Maze {
 				//for (int col =0;col<this.length;col++) {
 				//while(Welldown<(length*width-1)) {
 				//while any of the internal walls has true value
-					while(Welldown<(length*width+30000)) {	
+					while(CoordinateList2D2.size() > 1) {	
 					//0-4 for length of 5
 						Random ran = new Random();
 					int randomrow=ran.nextInt(length);
 					int randomcol=ran.nextInt(width);
 					int randomdir=ran.nextInt(4);
-					if (randomdir==0 && randomrow+1<width) {
-					if (CoordinateList2D[randomrow][randomcol]!=CoordinateList2D[randomrow+1][randomcol]&&
-							down[randomrow][randomcol]==true&&up[randomrow+1][randomcol]==true) {
-						//if there's more to the set, do it to the rest 
-						/*for (int row=0;row<width;row++) {
-							for (int col =0;col<length;col++) {
-							 if (CoordinateList2D[row][col]==CoordinateList2D[randomrow+1][randomcol]&&
-								row==randomrow+1) {;}
-							}}*/
-						
-						
-						CoordinateList2D[randomrow+1][randomcol]=CoordinateList2D[randomrow][randomcol];
-
-							//knock down the up wall of randomrow+1 and down wall of randomrow(same)
-
-						down[randomrow][randomcol]=false;
-						up[randomrow+1]	[randomcol]=false;	
-							
-							}
-
-					Welldown++;}
 					
-				if (randomdir==1&&(randomcol+1)<length) {
-				if (CoordinateList2D[randomrow][randomcol]!=CoordinateList2D[randomrow][randomcol+1]&&
-						right[randomrow][randomcol]==true&&left[randomrow][randomcol+1]==true) {
+					
+					
+					// RANDOM DIRECTION IS DOWN
+					if (randomdir==0 && randomrow+1<width) {
+					Coordinate myCoor;
+					myCoor = new Coordinate(randomcol,randomrow,'e','x',false,false,false,false);
+					Coordinate below = new Coordinate(randomcol,randomrow+1,'e','x',false,false,false,false);
+					for(Coordinate x:CoordinateList) {
+						if ((x.getX() == randomcol)&&(x.getY() == randomrow)) {
+							myCoor = x;
+						}
+						if ((x.getX() == randomcol)&&(x.getY() == randomrow + 1)) {
+							below = x;
+						}
 						
-					CoordinateList2D[randomrow][randomcol+1]=CoordinateList2D[randomrow][randomcol];
-						//knock down the right wall of randomcol and left wall of randomcol+1(same)
-					right[randomrow][randomcol]=false;
-					left[randomrow]	[randomcol+1]=false;
+					}
+					if ((myCoor.getDown() == true)&&(below.getUp() == true)&&(myCoor.getLetter() != below.getLetter())){
+						myCoor.setDown(false);
+						below.setUp(false);
+						char newLetter = myCoor.getLetter();
+						ArrayList<Coordinate> myList = new ArrayList<Coordinate>();
+						ArrayList<Coordinate> removeList = new ArrayList<Coordinate>();
+						for (ArrayList<Coordinate> y:CoordinateList2D2) {
+							if (y.contains(myCoor)) {
+								myList = y;
+							}
+							else if (y.contains(below)) {
+								removeList = y;
+							}
 						}
-	
-				Welldown++;}
-				if (randomdir==2&&randomrow-1>=0) {
-				if (CoordinateList2D[randomrow][randomcol]!=CoordinateList2D[randomrow-1][randomcol]&&
-						down[randomrow-1][randomcol]==true&&up[randomrow][randomcol]==true) {
-					CoordinateList2D[randomrow-1][randomcol]=CoordinateList2D[randomrow][randomcol];
-				//knock down the right wall of randomrow-1 and left wall of randomrow(same)
-					down[randomrow-1][randomcol]=false;
-					up[randomrow][randomcol]=false;		
+						if (removeList != myList) {
+						for(Coordinate c: removeList) {
+							c.setLetter(newLetter);
+							myList.add(c);
 						}
-
+						CoordinateList2D2.remove(removeList);}
+						
+						
+						
+					
+					
+						
+						
+					
+					}
+					Welldown++;
+					}
+					
+				
+				//RANDOM DIRECTION IS RIGHT
+				if (randomdir==1&&(randomcol+1)<length) {
+			
+					Coordinate myCoor;
+					myCoor = new Coordinate(randomcol,randomrow,'e','x',false,false,false,false);
+					Coordinate beside = new Coordinate(randomcol+1,randomrow,'e','x',false,false,false,false);
+					for(Coordinate x:CoordinateList) {
+						if ((x.getX() == randomcol)&&(x.getY() == randomrow)) {
+							myCoor = x;
+						}
+						if ((x.getX() == randomcol+1)&&(x.getY() == randomrow)) {
+							beside = x;
+						}
+						
+					}
+					if ((myCoor.getRight() == true)&&(beside.getLeft() == true)&&(myCoor.getLetter() != beside.getLetter())){
+						myCoor.setRight(false);
+						beside.setLeft(false);
+						char newLetter = myCoor.getLetter();
+						ArrayList<Coordinate> myList = new ArrayList<Coordinate>();
+						ArrayList<Coordinate> removeList = new ArrayList<Coordinate>();
+						for (ArrayList<Coordinate> y:CoordinateList2D2) {
+							if (y.contains(myCoor)) {
+								myList = y;
+							}
+							else if (y.contains(beside)) {
+								removeList = y;
+							}
+						}
+						if (removeList != myList) {
+						for(Coordinate c: removeList) {
+							c.setLetter(newLetter);
+							myList.add(c);
+						}
+						CoordinateList2D2.remove(removeList);}
+					
+					}
 				Welldown++;}
 				
-				if (randomdir==3&&randomcol-1>=0) {
-				if (CoordinateList2D[randomrow][randomcol-1]!=CoordinateList2D[randomrow][randomcol]&&
-						right[randomrow][randomcol-1]==true&&left[randomrow][randomcol]==true) {
-					CoordinateList2D[randomrow][randomcol-1]=CoordinateList2D[randomrow][randomcol];
-				//knock down the down wall of randomrow-1 and up wall of randomrow(same)
-					right[randomrow][randomcol-1]=false;
-					left[randomrow][randomcol]=false;	
+				
+				
+				
+				//RANDOM DIRECTION IS UP
+				if (randomdir==2&&randomrow-1>=0) {
+					Coordinate myCoor;
+					myCoor = new Coordinate(randomcol,randomrow,'e','x',false,false,false,false);
+					Coordinate above = new Coordinate(randomcol,randomrow-1,'e','x',false,false,false,false);
+					for(Coordinate x:CoordinateList) {
+						if ((x.getX() == randomcol)&&(x.getY() == randomrow)) {
+							myCoor = x;
 						}
-
+						if ((x.getX() == randomcol)&&(x.getY() == randomrow - 1)) {
+							above = x;
+						}
+						
+					}
+					if ((myCoor.getUp() == true)&&(above.getDown() == true)&&(myCoor.getLetter() != above.getLetter())){
+						myCoor.setUp(false);
+						above.setDown(false);
+						char newLetter = myCoor.getLetter();
+						ArrayList<Coordinate> myList = new ArrayList<Coordinate>();
+						ArrayList<Coordinate> removeList = new ArrayList<Coordinate>();
+						for (ArrayList<Coordinate> y:CoordinateList2D2) {
+							if (y.contains(myCoor)) {
+								myList = y;
+							}
+							else if (y.contains(above)) {
+								removeList = y;
+							}
+						}
+						if (removeList != myList) {
+						for(Coordinate c: removeList) {
+							c.setLetter(newLetter);
+							myList.add(c);
+						}
+						CoordinateList2D2.remove(removeList);}
+					
+					}
+					
+					
+					
+					
+				Welldown++;}
+				
+				
+				
+				
+				//RANDOM DIRECTION IS LEFT
+				if (randomdir==3&&randomcol-1>=0) {
+					Coordinate myCoor;
+					myCoor = new Coordinate(randomcol,randomrow,'e','x',false,false,false,false);
+					Coordinate beside = new Coordinate(randomcol-1,randomrow,'e','x',false,false,false,false);
+					for(Coordinate x:CoordinateList) {
+						if ((x.getX() == randomcol)&&(x.getY() == randomrow)) {
+							myCoor = x;
+						}
+						if ((x.getX() == randomcol-1)&&(x.getY() == randomrow)) {
+							beside = x;
+						}
+						
+					}
+					if ((myCoor.getLeft() == true)&&(beside.getRight() == true)&&(myCoor.getLetter() != beside.getLetter())){
+						myCoor.setLeft(false);
+						beside.setRight(false);
+						char newLetter = myCoor.getLetter();
+						ArrayList<Coordinate> myList = new ArrayList<Coordinate>();
+						ArrayList<Coordinate> removeList = new ArrayList<Coordinate>();
+						for (ArrayList<Coordinate> y:CoordinateList2D2) {
+							if (y.contains(myCoor)) {
+								myList = y;
+							}
+							else if (y.contains(beside)) {
+								removeList = y;
+							}
+						}
+						if (removeList != myList) {
+						for(Coordinate c: removeList) {
+							c.setLetter(newLetter);
+							myList.add(c);
+						}
+						CoordinateList2D2.remove(removeList);}
+					
+					}
 				Welldown++;	}
 				
-					} 
-				System.out.println(Welldown);
-
+				
+				} }
+				
+	public static void PrintMaze(ArrayList<Coordinate> list) {
+		Coordinate[][] ordered = new Coordinate[length][width];
+		for (int i = 0; i < length; i++){
+			for (int j = 0;j<width;j++) {
+				for (Coordinate c: list) {
+					if (c.getX() == i&&c.getY() == j) {
+						ordered[j][i] = c;
+					}
 				}
+			}
+		}
+		for (int i = 0; i < length; i++){
+			String bottom = "";
+			String row ="|";
+			String top ="";
+			for (int j = 0;j<width;j++) {
+				if (ordered[i][j].getUp() == true) {
+					top = top +" ---";}
+				 else {
+					 top = top +"    ";
+				}
+				row = row + " " + ordered[i][j].getLetter();
+				if (ordered[i][j].getRight() == true) {
+					row = row+" |";
+				 }
+				else { row= row+"  ";}
+				}
+			System.out.println(top);
+			System.out.println(row);
+			}
+		System.out.println(" --- --- --- --- --- --- --- --- --- --- --- --- --- ---     ");
+		}
+	
 
 	 public static void main(String[] args) 
 	    { 
-		 	Maze newMaze = new Maze(6,6);
+		 char c = 90;
+		 System.out.println(c);
+		 	Maze newMaze = new Maze(15,15);
 		 	//test - to print out status of each coordinates
 			for (int i=0;i<newMaze.CoordinateList.size();i++) {
 				System.out.println(newMaze.CoordinateList.get(i).toString());
-			
-			
-
 			}
-			/*for (int row=0;row<5;row++) {
-			for (int col =0;col<5;col++) {
-		        if((((col+1) % 5) == 0) ){
-		        	System.out.println(CoordinateList2D[row][col]);	
-		        }
-		        else{System.out.print(CoordinateList2D[row][col]);}
-		        
-			}*/
-			newMaze.PrintMaze(6,6);
+			newMaze.GenerateWalls();
+			for(ArrayList<Coordinate> a : newMaze.CoordinateList2D2) {
+				System.out.println(a.size());
+				for (Coordinate z: a) {
+					System.out.println(z.getLetter());
+					System.out.println(z.toString());
+					System.out.println(z.getRight());
+				}
+			}
+			newMaze.PrintMaze(newMaze.CoordinateList);
 			
-			}	
-	    //} 
-	 
-	 //Print out the actual wall shapes(-,|,|,-) 
-	 public static void PrintMaze(int length, int width) {
-		
-		 for(int i=0;i<width;i++) {
-			 String BottomWell ="";
-			 String Row ="|";
-			 String TopWell ="";
-			 for (int x=0;x<width;x++) {
-				 if (up[i][x]==true) {
-					 TopWell=TopWell+" ---";}
-					 else {
-						 TopWell=TopWell+"    ";
-					 		}
-				 Row=Row+ " " +CoordinateList2D[i][x];
-				
-				 
-				 if (right[i][x]==true){
-					
-					 Row=Row+" |";
-						 }
-				 else {Row=Row+"  ";}
-			 }
-			 System.out.println(TopWell);
-			 System.out.println(Row);
-			 
-			 //System.out.println(BottomWell);
-		 }
-		 System.out.println(" --- --- --- --- ---     ");
 
-		 /*
-		 LetterWitWalls=new String [length][width];
-			for (int row=0;row<width;row++) {
-			for (int col =0;col<length;col++) {
-				//make sure it switch row every count of length(e.g.5)
-				
-				//first part-go by row increase
-				//draw up up wall
-				if (up[row][col]==true){
-						
-					System.out.println("__");
-					}
-				else {
-					System.out.println(" ");
-					}
-				
-				//go down 1 row with println and draw Letter with left and right wall/space append to it
-				String LetterWithWall = "";
-				if (left[row][col]==true) {
-					LetterWithWall+="|";
-				}
-				else {
-					LetterWithWall+=" ";
-				}
-				LetterWithWall+=CoordinateList2D[row][col];
-				if (right[row][col]==true) {
-					LetterWithWall+="|";
-				}
-				else {
-					LetterWithWall+=" ";
-				}				
-				System.out.println(LetterWithWall);
-				//
-				
-				//draw  up wall
-				if (down[row][col]==true){
-					System.out.println("__");
-					}
-				else {
-					System.out.println(" ");
-					}
-				
-				
-				
-				}
-			}*/
-	 }
+	  
+	    } 
+
 }
