22c7159245eca57847f47b8c1e02e4b7cc228514
nfatima-iqbal <60020406+nfatima-iqbal@users.noreply.github.com>
2020-03-31 10:25:45 -0600

    Fixed. Previously, wrong file was uploaded

--- AlertBox.java
+++ AlertBox.java
@@ -1,19 +1,17 @@
 /**
  * @author T06 Group 4
- * @version Demo 2 GUI based game
- * @implNote This class creates a congratulatory alert box when the player wins the game.
+ * @version Demo 3 GUI based game
+ * @implNote This class creates a congratulatory alert box when the player wins the game, and a different alert box when
+ * 			the player looses the game. This will be edited for the Interactive demo to also state the score when the player
+ * 			wins.
  */
 package application;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 
 
 public class AlertBox {
@@ -21,6 +19,9 @@ public class AlertBox {
 	static ImageView knucklesLoss = new ImageView("knucklesloss.gif");
 	static ImageView MSWin = new ImageView("MSwin.gif");
 	static ImageView MSLoss = new ImageView("MSLoss.gif");
+	/*
+	 * This creates a customized loss screen. It is called when the player looses all their lives. 
+	 */
 	public static void displayLoss(int character){
 		if (character == 2){
 		Stage window = new Stage();
@@ -39,8 +40,11 @@ public class AlertBox {
 		window.setScene(scene);
 		window.showAndWait();
 		}
-		
 	}
+	/*
+	 * This creates a customized win screen. It is called when the player successfully completes the maze. This
+	 * will be edited for the Interactive Demo to also include the score message.  
+	 */
 	public static void displayWin(int character) {
 		
 		if (character == 2){
